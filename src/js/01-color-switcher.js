133f2b6f26586be1e9465e699f4c68afc4ae7adc
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-05-06 19:26:50 +0300

    task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let interval;
+
+startBtn.addEventListener('click', () => {
+  interval = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
